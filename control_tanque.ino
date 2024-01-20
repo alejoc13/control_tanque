aebe11a44a06963ae7320d522fded9807a5cdcc3
Johan Alejandro Castrillon <alejocastrillon13@gmail.com>
2024-01-20 13:26:01 -0500

    Clean some prints

--- control_tanque.ino
+++ control_tanque.ino
@@ -1,14 +1,16 @@
 const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
 const int Echo = 3;   //Pin digital 3 para el Echo del sensor
- 
+const int Solenoid = 4;
 void setup() {
   Serial.begin(9600);//iniciailzamos la comunicación
   pinMode(Trigger, OUTPUT); //pin como salida
   pinMode(Echo, INPUT);  //pin como entrada
+  pinMode(Solenoid,OUTPUT);
   digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+  digitalWrite(Solenoid, LOW);
 }
 
-void distance_control(int time_review){
+int distance_control(){
   long t; //timepo que demora en llegar el eco
   long d; //distancia en centimetros
  
@@ -21,12 +23,13 @@ void distance_control(int time_review){
   
   Serial.print("Distancia: ");
   Serial.print(d);      //Enviamos serialmente el valor de la distancia
-  Serial.print("cm");
+  Serial.print("cm ");
   Serial.println();
-  delay(time_review);          //Hacemos una pausa de 100ms
+  return d;
 }
 
 void loop(){
-  distance_control(1000);
+  int distance = distance_control();
+  delay(1000);
   
 }
